2b23259a1059acbd031063b98c9d469b89e57df1
alexandrosdrymonitis <adrcki@gmail.com>
2014-11-21 17:57:29 +0200

    Update pin_states.h

--- pin_states.h
+++ pin_states.h
@@ -4,6 +4,6 @@
 // for previous and current state comparison
 byte banana_pins[NUM_OF_MODULES] = { B00000001, B00000001, B00000011 };
 
-// array to hold binary number of input chips pins with switches only
+// array to hold binary numbers of input chips pins with switches only
 // for previous and current state comparison
 byte switch_pins[NUM_OF_MODULES] = { B00000010, B00000110, B00001100 };
