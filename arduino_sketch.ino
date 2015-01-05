1d88616e93463285347e7bdfa915bb0b9266196e
alexandrosdrymonitis <adrcki@gmail.com>
2015-01-05 19:11:53 +0200

    Update and rename arduino_code.ino to arduino_sketch.ino

--- arduino_sketch.ino
+++ arduino_sketch.ino
@@ -1,43 +1,26 @@
 // code to be devoloed for the modular synthesizer based on Pd, Teensy 3.1 and Odroid
+// goes with modular_synth_test Pd patch
 // written by Alexandros Drymonitis
 
 #include <SPI.h>
 #include "pin_states.h"
 
-// set macros
-
-#define MASTER_CONTROL0 5
-#define MASTER_CONTROL1 4
-#define MASTER_CONTROL2 3
-#define MASTER_CONTROL3 2
-
-#define SLAVE_CONTROL0 8
-#define SLAVE_CONTROL1 7
-#define SLAVE_CONTROL2 6
-
-#define NUM_OF_MODULES 3
-
-#define SMOOTH 20
-
-// global variables with pointers
+// global variables
 
 // set global variables for output shift registers
 const byte output_latch = 10;
-//const byte *output_latch_pointer = &output_latch;
 // byte array to transfer data to output chips via SPI
 byte output_data[NUM_OF_MODULES] = { 0 };
 // array to hold number of banana pins used of each output chip
-byte output_pins[NUM_OF_MODULES] = {6, 3, 3};
+byte output_pins[NUM_OF_MODULES] = {7, 3, 3};
 // array to hold the position of the first LED of each module
 // if an LED is controlled over serial, exclude it
-byte led_pos[NUM_OF_MODULES] = {6, 3, 4};
+byte led_pos[NUM_OF_MODULES] = {7, 3, 4};
 // boolean to exit the output pins loop when a new connection has been detected
-boolean terminate;
-//boolean *terminate_pointer = &terminate;
+boolean terminate_bananas;
 
 // set global variables for input shift registers
 const byte input_latch = 9;
-//const byte *input_latch_pointer = &input_latch;
 // array to receive data from input chips via SPI
 byte input_data[NUM_OF_MODULES];
 // array to hold position of first switch of each module, starting from 0
@@ -45,16 +28,24 @@ byte input_data[NUM_OF_MODULES];
 byte switch_pos[NUM_OF_MODULES] = {1, 1, 3};
 // array to hold number of switches on each module
 // exclude switches that don't control LEDs
-byte num_of_switches[NUM_OF_MODULES] = {1, 2, 1};
+byte num_of_switches[NUM_OF_MODULES] = {1, 1, 1};
 // two-dimensional array to hold states of banana plugs
 // row number = sum of elements of outputPins, column number = NUM_OF_MODULES
-byte banana_states[12][NUM_OF_MODULES];
+byte banana_states[13][NUM_OF_MODULES];
 // array to store state of switches
 byte switch_states[NUM_OF_MODULES];
+// and this is the index of the ADC/DAC module, where this switch is attached to
+int dac_module = 2;
+// boolean to determine whether to store connection states to active_modules or back_up array
+boolean patch_boolean = false;
 
-// variable to hold the DSP state to control the DSP LED
+// indices for connection and switch functions
+const byte connection_index = NUM_OF_MODULES;
+const byte switch_index = connection_index + 1;
+
+// variable to receive data from the serial port
+byte serial_value;
 byte DSPstate = 0;
-//byte *DSPstate_pointer = &DSPstate;
 
 // set global variables for multiplexers
 // master multiplexers controlling the multiplexers of each module
@@ -63,29 +54,30 @@ const byte num_of_master_mux = 1;
 int num_of_slave_mux[num_of_master_mux] = {3};
 // two dimensional array to hold number of pins used on each slave multiplexer
 // rows = num_of_master_mux, columns = greatest number in num_of_slave_mux array
-int num_of_pots[num_of_master_mux][3] = { { 7, 4, 1 } };
-const int total_pots = 12; // sum of elements of num_of_pots
+int num_of_pots[num_of_master_mux][3] = { { 8, 4, 1 } };
+const int total_pots = 13; // sum of elements of num_of_pots
 // array to store multiple readings of each knob for smoothing
 unsigned int multiple_pots[total_pots][SMOOTH];
-// array to sum up reading for smoothing
+// array to sum up readings for smoothing
 unsigned long sums[total_pots];
 // column variable to control multiple_pots
 static byte tail;
+// arrays to store and back up activity of modules (back up if we're not updating the patching)
+byte active_modules[NUM_OF_MODULES];
+byte back_up_modules[NUM_OF_MODULES];
 
-// indices for switches function and potentiometers
-int switch_index = 5;
-//int *switch_index_pointer = &switch_index;
-int pot_index = switch_index + 3;
-//int *pot_index_pointer = &pot_index;
+// global index for connection, switch and potentiometer functions and loops
+int ndx;
 
-// number of bytes to be transfered to Pure Data over serial
-// this number is totalPots * 2
-// plus four for the banana plugs function
-// plus three for the switches function
+// maximum number of bytes to be transfered to Pure Data over serial
+// 99% of the times only part of this will be transferred to Pd
+// this number is totalPots * 2 plus total number of modules (NUM_OF_MODULES macro)
+// plus five for the connections function
+// plus four for the switches function
 // plus one (for denoting the beginning of data)
-const byte num_of_transfer_data = (total_pots * 2) + 8;
+const byte num_of_total_data = (total_pots * 2) + NUM_OF_MODULES + 10;
 // buffer to hold data transfered to Pure Data over serial
-byte transfer_data[num_of_transfer_data];
+byte transfer_data[num_of_total_data];
 
 
 // Custom functions
@@ -109,42 +101,70 @@ void refresh_input()
 }
 
 // function to check if any connection has changed
-void check_connections(int pin)
+void check_connections(int pin, int module)
 {
-  int index = 1;
+  int local_index = ndx;
   // store the first altered state of input bytes to the transfer array
   for(int i = 0; i < NUM_OF_MODULES; i++){
     // mask the input byte to exclude any possible switches on the module and check if changed
     byte masked_banana = input_data[i] & banana_pins[i];
     if(masked_banana != banana_states[pin][i]){
-      // store input byte, output pin and input chip # to the transfer array
-      transfer_data[index++] = input_data[i] & 0x7f;
-      transfer_data[index++] = input_data[i] >> 7;
-      transfer_data[index++] = pin;
-      transfer_data[index] = i;
+      // store connection data index, input byte, output pin and input chip # to the transfer array
+      transfer_data[local_index++] = connection_index;
+      transfer_data[local_index++] = input_data[i] & 0x7f;
+      transfer_data[local_index++] = input_data[i] >> 7;
+      transfer_data[local_index++] = pin;
+      transfer_data[local_index++] = i;
+      // check if the current module is being activated or not
+      if(masked_banana != banana_pins[i]){
+        if(module == i) back_up_modules[i] += 1;
+        else{
+          back_up_modules[i] += 1;
+          back_up_modules[module] += 1; 
+        }
+      }
+      else{
+        if(module == i) back_up_modules[i] -= 1;
+        else{
+          back_up_modules[i] -= 1;
+          back_up_modules[module] -= 1;
+        }
+      }
       // update the banana_states array
       banana_states[pin][i] = masked_banana;
-      // set the loop termination boolean to true and exit loop
-      terminate = true;
+      // set the loop termination boolean to true, update the global index and exit loop
+      terminate_bananas = true;
+      ndx = local_index;
       break;
     }
+  }
+  // update the active_modules array if the patch update switch is on
+  if(patch_boolean){
+    for(int i = 0; i < NUM_OF_MODULES; i++) active_modules[i] = back_up_modules[i];
   }
 }
 
 // function to check if the state of any switch has changed
 void check_switches()
 {
-  int index = switch_index;
+  int local_index = ndx;
   for(int i = 0; i < NUM_OF_MODULES; i++){
     // mask the input byte according to the position of the switches and check if changed
     byte masked_switch = input_data[i] & switch_pins[i];
     if(masked_switch != switch_states[i]){
-      // if changed, store to transfer array which chip and the corresponding byte
-      transfer_data[index++] = input_data[i] & 0x7f;
-      transfer_data[index++] = input_data[i] >> 7;
-      transfer_data[index] = i;
-      // update the switch_states array and exit loop
+      // if changed, store switch data index, chip # and the corresponding byte
+      transfer_data[local_index++] = switch_index;
+      transfer_data[local_index++] = input_data[i] & 0x7f;
+      transfer_data[local_index++] = input_data[i] >> 7;
+      transfer_data[local_index++] = i;
+      // check if we're in the dac_module and read the patch update switch
+      if(i == dac_module){
+        if((masked_switch & patch_update) == patch_update) patch_boolean = false;
+        else patch_boolean = true;
+      }
+      // update the switch_states array and the global index and exit loop
       switch_states[i] = masked_switch;
+      ndx = local_index;
       break;
     }
   }
@@ -172,7 +192,7 @@ void setup()
   refresh_output();
   
   //initialize the banana sockets states two-dimensional array
-  for(int i = 0; i < 12; i++){
+  for(int i = 0; i < 13; i++){
     for(int j = 0; j < NUM_OF_MODULES; j++)
       banana_states[i][j] = banana_pins[j];
   }
@@ -180,9 +200,6 @@ void setup()
   // initialize the switch states array
   for(int i = 0; i < NUM_OF_MODULES; i++)
     switch_states[i] = switch_pins[i];
-    
-  for(int i = 0; i < 7; i++)
-    transfer_data[i] = 0xff;
   
   // initialize Arduino's control pins for multiplexers
   for(int i = 2; i < 9; i++){
@@ -199,11 +216,17 @@ void setup()
 void loop()
 {
   transfer_data[0] = 0xc0; // denote start of data stream
+  ndx = 1;
   
   // store DSP state input from Pd
   if(Serial.available()){
-    DSPstate = Serial.read() - '0';
-    bitWrite(output_data[2], 3, DSPstate);
+    byte inByte = Serial.read();
+    if((inByte >= '0') && (inByte <= '9'))
+      serial_value = inByte - '0';
+    else{
+      if(inByte == 'd') DSPstate = serial_value;
+    }
+    bitWrite(output_data[dac_module], 3, DSPstate);
   }
   
   // set all LEDs according to switches
@@ -219,76 +242,76 @@ void loop()
   
   // write to output shift registers and read input shift registers
   // first set terminate boolean to false
-  terminate = false;
+  terminate_bananas = false;
   // pin number variable
   int pin = 0;
-  // update the shift registers
   for(int i = 0; i < NUM_OF_MODULES; i++){
     // add number of pins of previous chip
     if(i) pin += output_pins[i - 1];
     // variable to control the bits of the current byte
     int bit_increment = 0;
-    // then go through the banaba plug pins; start loop from 1 to set the pin variable correctly
+    // then go through the banaba plug pins
     for(int j = 0; j < output_pins[i]; j++){
-      // local variable to store pin number
       int local_pin = pin;
       // set the current pin LOW
       bitClear(output_data[i], bit_increment);
-      // call the output shift registers function
       refresh_output();
       // give some time to the shift registers to do their job
       delayMicroseconds(1);
-      // call the input shift registers function
       refresh_input();
       // add current and previous pins of current chip
       local_pin += j;
-      // check if there's a new connection
-      check_connections(local_pin);
+      check_connections(local_pin, i);
       // reset the pin HIGH
       bitSet(output_data[i], bit_increment);
       // check if a new connection has been detected and if so, exit loop
-      if(terminate) break;
+      if(terminate_bananas) break;
       // increment byte's bit
       bit_increment++;
     }
-    if(terminate) break;
+    if(terminate_bananas) break;
   }
   
-  // call the function that stores altered states of switches
   check_switches();
   
-  int index = pot_index; // transfer data array index offset
-  // read potentiometers
+  // set local index to global index's current value
+  int local_index = ndx;
+  // set row index for 2D array multiple pots
   int row_index = 0;
+  int module_index = 0;
   // run througn all master multiplexers
   for(int master_mux = 0; master_mux < num_of_master_mux; master_mux++){
-    // first move the elements of current row one position to the left so we can add the new value at the end
-    //for(int i = 0; i < last_col; i++) multiple_pots[row_index][i] = multiple_pots[row_index][i + 1];
     // run through all slave multiplexers
     for(int slave_mux = 0; slave_mux < num_of_slave_mux[master_mux]; slave_mux++){
       digitalWrite(MASTER_CONTROL0, (slave_mux&15)>>3);
       digitalWrite(MASTER_CONTROL1, (slave_mux&7)>>2);
       digitalWrite(MASTER_CONTROL2, (slave_mux&3)>>1);
       digitalWrite(MASTER_CONTROL3, (slave_mux&1));
+      // if this module is active, store its index to the transfer array
+      if(active_modules[module_index]) transfer_data[local_index++] = module_index;
       // run through the pins used on each slave multiplexer
       for(int pot = 0; pot < num_of_pots[master_mux][slave_mux]; pot++){
-        digitalWrite(SLAVE_CONTROL0, (pot&7)>>2);
-        digitalWrite(SLAVE_CONTROL1, (pot&3)>>1);
-        digitalWrite(SLAVE_CONTROL2, (pot&1));
-        sums[row_index] -= multiple_pots[row_index][tail];
-        sums[row_index] += multiple_pots[row_index][tail] = analogRead(master_mux);
-        // call the smoothing function
-        unsigned int smoothed = sums[row_index] / SMOOTH;
-        // and store the smoothed value to the transfer_data array
-        transfer_data[index++] = smoothed & 0x007f;
-        transfer_data[index++] = (smoothed >> 7) & 0x003f;
-        row_index++;
+        if(active_modules[module_index]){ // if this module is active, read its pots
+          digitalWrite(SLAVE_CONTROL0, (pot&7)>>2);
+          digitalWrite(SLAVE_CONTROL1, (pot&3)>>1);
+          digitalWrite(SLAVE_CONTROL2, (pot&1));
+          // smooth out the analog reading
+          sums[row_index] -= multiple_pots[row_index][tail];
+          sums[row_index] += multiple_pots[row_index][tail] = analogRead(master_mux);
+          unsigned int smoothed = sums[row_index] / SMOOTH;
+          // and store the smoothed value to the transfer_data array
+          transfer_data[local_index++] = smoothed & 0x007f;
+          transfer_data[local_index++] = (smoothed >> 7) & 0x003f;
+        }
+        row_index++; // update the 2D array row index anyway
       }
+      module_index++;
     }
   }
   tail++;
   if(tail >= SMOOTH) tail = 0;
   
   // write all data to the serial port
-  Serial.write(transfer_data, num_of_transfer_data);
+  int current_data = local_index; // set amount of data to transfer
+  Serial.write(transfer_data, current_data);
 }
